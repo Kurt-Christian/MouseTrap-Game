79ec4b5f070f438b763f41a7597d881b56a6b50e
Kurt Krenz <kekrenz@me.com>
2016-02-03 15:13:04 -0500

    Create MouseTrapInterface.java

---
+++ MouseTrapInterface.java
@@ -0,0 +1,21 @@
+public interface MouseTrapInterface
+{
+	/**
+	 * 
+	 * @return
+	 */
+	public MouseTrap armed();
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public MouseTrap sprung();
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public MouseTrap disabled();
+
+}
